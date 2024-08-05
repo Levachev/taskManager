f82e27f1eca3d0921df215fe06cdae1a8bf4bd4a
r.levachev <r.levachev@g.nsu.ru>
2024-08-05 11:37:14 +0700

    first commit

---
+++ src/main/java/com/example/takManager/TakManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.takManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakManagerApplication.class, args);
+	}
+
+}
